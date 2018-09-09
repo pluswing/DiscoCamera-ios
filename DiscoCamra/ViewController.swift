9bd22446269851622966a31d5ebe213a42f149e8
齋藤敏行 <toshiyuki.saito.16@gmail.com>
2018-09-09 16:01:53 +0900

    Initial Commit

---
+++ DiscoCamra/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DiscoCamra
+//
+//  Created by pluswing on 2018/09/09.
+//  Copyright © 2018年 pluswing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
